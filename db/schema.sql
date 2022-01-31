f4587b92fd647948e011a5a1ebed51045825019b
Tanner Brieger <tanner29@gmail.com>
2022-01-30 21:42:15 -0600

    fixed typo, changed some info on seeds

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,20 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 
+CREATE TABLE departments (
+    id INTEGER AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL (8,2),
+    department_id INTEGER (10),
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE employees (
     id INTEGER AUTO_INCREMENT,
     first_name VARCHAR(30),
@@ -14,21 +28,7 @@ CREATE TABLE employees (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE roles (
-    id INTEGER AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary DECIMAL (30.2),
-    department_id INTEGER (10),
-    PRIMARY KEy (id)
-);
 
-CREATE TABLE departments (
-    id INTEGER AUTO_INCREMENT,
-    name VARCHAR(30),
-    PRIMARY KEY (id)
-);
-
-
-SELECT * FROM employees;
-SELECT * FROM roles;
 SELECT * FROM departments;
+SELECT * FROM roles;
+SELECT * FROM employees;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
-  ('Ronald', 'Firbank', 2, 1),
-  ('Virginia', 'Woolf', 1, 2),
+  ('Ronald', 'Firbank', 1, 2),
+  ('Virginia', 'Woolf', 2, 2),
   ('Piers', 'Gaveston', 3, 2);
 
 
@@ -10,7 +10,7 @@ INSERT INTO roles (title, salary, department_id)
 VALUES
     ('Manager', 175, 1),
     ('Engineer', 100, 2),
-    ('Intern', 8, 2);
+    ('Intern', 8, 3);
 
 INSERT INTO departments (name)
 VALUES
